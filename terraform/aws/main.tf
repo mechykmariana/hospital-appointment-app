05e29218a88b54bfe740beb4aea8a2fb97c5c66e
mechykmariana <maryanamechik3@gmail.com>
2025-04-05 22:10:17 +0000

    testing aws

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "allow_http" {
     name = "allow-http"
     description = "allow http and ssh traffic"
 
-    ingress = {
+    ingress {
         from_port = 22
         to_port = 22
         protocol = "tcp"
